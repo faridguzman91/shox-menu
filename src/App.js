2b536e9a7408ac4150f565ff83ac856799573c70
Jimmy Guzman <jimmy@creapolis.com>
2021-10-02 11:22:56 +0200

    burger .js global styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './global';
+import { theme } from  './theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+      <GlobalStyles />
+      <div>
+        <h1>menu</h1>
+        <img src="https://schc.nl/files/nieuws/LOGO%20III_BLACK.PNG" alt="burger icon" />
+        <small>author: Farid Guzman</small>
+      </div>
+      </>
+      
+    </ThemeProvider>
   );
 }
 
---
+++ src/global.js
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 10px;
+  }
+  *, *::after, *::before {
+    box-sizing: border-box;
+  }
+  body {
+    align-items: center;
+    background: ${({ theme }) => theme.primaryLight};
+    color: ${({ theme }) => theme.primaryDark};
+    display: flex;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    height: 100vh;
+    justify-content: center;
+    text-rendering: optimizeLegibility;
+  }
+  ;
+
+  h1 {
+    font-size: 2rem;
+    text-align: center;
+    text-transform: uppercase;
+  }
+  img {
+    border-radius: 5px;
+    height: 300px;
+    width: auto;
+  }
+  div {
+    text-align: center;
+  }
+  small {
+    display: block;
+  }
+  a {
+    color: ${({ theme }) => theme.primaryHover};
+    text-decoration: none;
+  }
+`;
